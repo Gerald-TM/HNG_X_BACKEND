9af70725ebed532aa1a01dc46c19404027c93f57
Gerald__TM <taiyemusa1582@gmail.com>
2023-09-08 00:53:15 +0100

    Added comments

--- endpoint/views.py
+++ endpoint/views.py
@@ -8,14 +8,20 @@ import json
 
 
 def get_endpoint_view(request):
+    # The GET request query parameters; slack_name and track
     slack_name = request.GET.get("slack_name")
     track = request.GET.get("track")
 
+    # validate the GET query parameters for slack_name and track.
     if slack_name == 'gerald__tm' and track == 'backend':
 
-        current_day = datetime.now().strftime('%A')
+        # Current day of the week.
+        current_day = datetime.utcnow().strftime("%A")
+
+        # Current UTC time (with validation of +/-2)
         utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
 
+        # Response data in the form of a python dictionary
         response_data = {
             'slack_name': slack_name,
             'current_day': current_day,
@@ -27,11 +33,15 @@ def get_endpoint_view(request):
 
         }
 
-        return HttpResponse('\t\tHNG_X Task: Create and host an endpoint.\n\n' + json.dumps(response_data, indent=3), content_type="application/json")
+        # Convert the python dictionary to a JSON formatted string and return the data as a regular HttpResponse.
+        return HttpResponse('\t\tHosted endpoint taking in two GET request query parameter, and returning specific information in JSON format.\n\n' +
+                             json.dumps(response_data, indent=3), content_type="application/json")
 
     else:
+        # Return an error if the GET request query parameters fail to be validated or correct.
         response_data = {
             'Error': "You have to pass in GET query parameters 'slack_name' and 'track' e.g URL?slack_name=your_slack_name&track=your_track"
         }
 
+        # Return the error message as HttpResponse of a JSON formatted string.
         return HttpResponse(json.dumps(response_data, indent=3), content_type="application/json")
